947a8d57cd702a9d9b0200024122268f5a80180c
James Bennett <jamespjbennett@hotmail.com>
2017-03-05 22:46:17 +0000

    remove rack cors line from development.rb

--- creative_rooms_master/config/environments/development.rb
+++ creative_rooms_master/config/environments/development.rb
@@ -31,12 +31,12 @@ Rails.application.configure do
   config.autoload_paths += %W["#{config.root}/app/validators/"]
 
 
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
-    end
-  end
+  # config.middleware.insert_before 0, Rack::Cors do
+  #   allow do
+  #     origins '*'
+  #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+  #   end
+  # end
   config.action_mailer.perform_caching = false
 
 
