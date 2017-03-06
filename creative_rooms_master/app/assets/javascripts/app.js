15cd1a81765aa7609c1ee7a30405861d78835e38
James Bennett <jamespjbennett@hotmail.com>
2017-03-06 13:26:09 +0000

    angular hooked up properly

--- creative_rooms_master/app/assets/javascripts/app.js
+++ creative_rooms_master/app/assets/javascripts/app.js
@@ -4,7 +4,36 @@ angular.module('creativeRooms', ['ui.router', 'templates', 'Devise'])
 	'$urlRouterProvider',
 	'$locationProvider',
 	function($stateProvider, $urlRouterProvider, $locationProvider) {
-
+	$stateProvider
+	    .state('home', {
+	      url: '/home',
+		  templateUrl: 'home/_home.html',
+	      controller: 'MainCtrl',
+	    })
+	    .state('login', {
+	      onEnter: ['$state', 'Auth', function($state, Auth) {
+	        Auth.currentUser().then(function (){
+	          $state.go('home');
+	        })
+	      }],
+	      url: '/login',
+	      templateUrl: 'auth/_login.html',
+	      controller: 'AuthCtrl'
+	    })
+	    .state('register', {
+	      onEnter: ['$state', 'Auth', function($state, Auth) {
+	        Auth.currentUser().then(function (){
+	          $state.go('home');
+	        })
+	      }],
+	      url: '/register',
+	      templateUrl: 'auth/_register.html',
+	      controller: 'AuthCtrl'
+	    });
+	$urlRouterProvider.otherwise(function($injector) {
+		var $state = $injector.get('$state');
+		$state.go('home');
+	});
 	$locationProvider.html5Mode({
 		enabled: true,
 		requireBase: false
---
+++ creative_rooms_master/app/assets/javascripts/home/mainCtrl.js
@@ -0,0 +1,21 @@
+angular.module('creativeRooms')
+.controller('MainCtrl', [
+'$scope', 'posts',
+function($scope, posts){
+  	$scope.test = 'Hello world!';
+  	$scope.posts = posts.posts;
+	$scope.addPost = function(){
+	  if(!$scope.title || $scope.title === '') { return; }
+	  posts.create({
+	    title: $scope.title,
+	    link: $scope.link,
+	  });
+	  $scope.title = '';
+	  $scope.link = '';
+
+	  $scope.posts = posts.posts;
+	};
+	$scope.incrementUpvotes = function(post) {
+	  posts.upvote(post);
+	};
+}])
