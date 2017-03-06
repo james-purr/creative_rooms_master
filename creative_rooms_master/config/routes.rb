80e4c0c7abded080aaa113eb198c543145f9e0f5
James Bennett <jamespjbennett@hotmail.com>
2017-03-06 18:56:18 +0000

    slight refactoring

--- creative_rooms_master/config/routes.rb
+++ creative_rooms_master/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#angular'
-  authenticated do
-    root to: "application#angular", as: :authenticated_root
-  end
+	devise_for :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'application#angular'
+	authenticated do
+		root to: "application#angular", as: :authenticated_root
+	end
+
+	match '/home' => 'application#angular', via: [:get]
+	match '/login' => 'application#angular', via: [:get]
+	match '/register' => 'application#angular', via: [:get]
 end
