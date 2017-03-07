f47e3e957635b179b61c7df5df645e7b6d2165cd
James Bennett <jamespjbennett@hotmail.com>
2017-03-07 19:01:10 +0000

    catch form errors and append

--- creative_rooms_master/app/assets/javascripts/auth/authCtrl.js
+++ creative_rooms_master/app/assets/javascripts/auth/authCtrl.js
@@ -4,6 +4,7 @@ angular.module('creativeRooms')
 '$state',
 'Auth',
 function($scope, $state, Auth){
+    $scope.errors = null;
     $scope.itemArray = [
         {id: 0, name: 'Homeowner'},
         {id: 1, name: 'Artist'},
@@ -20,9 +21,17 @@ function($scope, $state, Auth){
   $scope.register = function() {
 
     $scope.user.role_enum = $scope.user.role_enum.id
-    Auth.register($scope.user).then(function(){
-      debugger
+    Auth.register($scope.user)
+    .then(function(){ 
       $state.go('home');
+    })
+    .catch(function (data) {
+        var error = "";
+        for (var i = 0; i < Object.keys(data.data.errors).length; i++) {
+          var key = Object.keys(data.data.errors)[i]
+          error = error + key.replace("_", " ") + " " + data.data.errors[key]
+        };
+        $scope.errors = error;
     });
 
   };
