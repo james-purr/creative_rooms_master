6d7a259393831594ad3cafeacfaa07620c04e7eb
James Bennett <jamespjbennett@hotmail.com>
2017-03-09 07:37:04 +0000

    fixed the password match validation

--- creative_rooms_master/app/assets/javascripts/directives/passwordMatch.js
+++ creative_rooms_master/app/assets/javascripts/directives/passwordMatch.js
@@ -1,24 +1,33 @@
-// angular.module('creativeRooms')
-// .directive('validPasswordC', function() {
-//   return {
-//     require: 'ngModel',
-//     scope: {
+angular.module('creativeRooms')
+.directive("passwordVerify", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            passwordVerify: '='
+        },
+        link: function(scope, element, attrs, ctrl) {
 
-//       reference: '=validPasswordC'
-
-//     },
-//     link: function(scope, elm, attrs, ctrl) {
-//       ctrl.$parsers.unshift(function(viewValue, $scope) {
-
-//         var noMatch = viewValue != scope.reference
-//         ctrl.$setValidity('noMatch', !noMatch);
-//         return (noMatch)?noMatch:!noMatch;
-//       });
-
-//       scope.$watch("reference", function(value) {;
-//         ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
-
-//       });
-//     }
-//   }
-// });
+            scope.$watch(function() {
+                var combined;
+                
+                if (scope.passwordVerify || ctrl.$viewValue) {
+                   combined = scope.passwordVerify + '_' + ctrl.$viewValue; 
+                }                    
+                return combined;
+            }, function(value) {
+                if (value) {
+                    ctrl.$parsers.unshift(function(viewValue) {
+                        var origin = scope.passwordVerify;
+                        if (origin !== viewValue) {
+                            ctrl.$setValidity("passwordVerify", false);
+                            return undefined;
+                        } else {
+                            ctrl.$setValidity("passwordVerify", true);
+                            return viewValue;
+                        }
+                    });
+                }
+            });
+        }
+    };
+});
