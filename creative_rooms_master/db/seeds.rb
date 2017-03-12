641fa864671d47edec0e0cdc486fd91a755efcb2
James Bennett <jamespjbennett@hotmail.com>
2017-03-12 20:40:03 +0000

    creating some seed data for properties

--- creative_rooms_master/db/seeds.rb
+++ creative_rooms_master/db/seeds.rb
@@ -6,6 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+require 'ffaker'
+
 20.times do
-	User.create(first_name: 'james', last_name: 'bennett', email: 'sfgsfg@sdg.com', password: 'password', password_confirmation: 'password', role_enum:0)
+	User.create(first_name: FFaker::Name.first_name, 
+				last_name: FFaker::Name.last_name, 
+				email: FFaker::Internet.email, 
+				password: 'password', 
+				password_confirmation: 'password', 
+				role_enum:0
+	)
+end
+
+property_owners = User.where(role_enum: 0);
+
+postcode_array = [
+'E17 6AY',
+'E17 7NW',
+'E17 8PF',
+'E17 9HZ',
+'E17 5NS',
+'E17 5RR',
+'E17 8HP',
+'E17 9QH',
+'E17 3QW',
+'E17 8PY',
+'E17 3DX',
+'E17 5RA',
+'E17 3PH',
+'E17 3SR',
+'E17 6EF',
+'E17 3RQ',
+'E17 5NP',
+'E17 6QW',
+'E17 9BL',
+'E17 8NN',
+'E17 5LE',
+'E17 8LT',
+'E17 6RN',
+'E17 5DZ',
+'E17 8PQ',
+'E17 8AY',
+'E17 3HA',
+'E17 6LB',
+'E17 6EJ',
+'E17 7QD',
+'E17 5DZ',
+'E17 4RQ',
+'E17 9QQ',
+'E17 4LE',
+'E17 9JY',
+'E17 5NZ',
+'E17 6QS',
+'E17 5AZ',
+'E17 3BB',
+'E17 8LW',
+'E17 4RX',
+'E17 3LN',
+'E17 4NS',
+'E17 3HQ',
+'E17 4LP',
+'E17 5PE',
+'E17 8LW',
+'E17 8HH'
+]
+
+
+(0..37).each do |number|
+	property = Property.create(postcode: postcode_array.sample, description: FFaker::Lorem.paragraph, space_for_artist: [true, false].sample, user_id: property_owners.sample.id)
+
+	pi = PropertyImage.create(property_id: property.id, image: File.new("#{Rails.root}/app/assets/images/property_images/" + number + ".jpg"))
+
 end
