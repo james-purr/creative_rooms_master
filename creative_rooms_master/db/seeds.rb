52990079b331e8ae9f4f3a479fff9e48812a0129
James Bennett <jamespjbennett@hotmail.com>
2017-03-12 20:01:52 +0000

    a bunch of property images to seed with

--- creative_rooms_master/db/seeds.rb
+++ creative_rooms_master/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+	User.create(first_name: 'james', last_name: 'bennett', email: 'sfgsfg@sdg.com', password: 'password', password_confirmation: 'password', role_enum:0)
+end
