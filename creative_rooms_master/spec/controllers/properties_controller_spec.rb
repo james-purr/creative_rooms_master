e19d4d3c2e9d745f40c62b792ec39ebc777886c5
James Bennett <jamespjbennett@hotmail.com>
2017-03-13 19:24:56 +0000

    changed up controller test for getting featured properties

--- creative_rooms_master/spec/controllers/properties_controller_spec.rb
+++ creative_rooms_master/spec/controllers/properties_controller_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe PropertiesController, type: :controller do
 
 	let(:parsed_response) { response.body.to_json }
-	
+
 	describe "GET #featured" do
+    	user = User.create(first_name: 'test', last_name: 'test', email: Faker::Internet.email, password: 'password',  password_confirmation: 'password', role_enum: 0)
 		let!(:property) { Property.create(postcode: Faker::Address.zip_code, description: 'test desc', space_for_artist: true, user_id: user.id, featured:true) }
 		let!(:property_2) { Property.create(postcode: Faker::Address.zip_code, description: 'test desc', space_for_artist: true, user_id: user.id, featured:false) }
 
 		before :each do
 			get :featured
 		end
-	  expect(parsed_response.first["id"].to_i).to eq property.id
+		it 'should return featured properties' do
+	  		expect(parsed_response.first["id"].to_i).to eq property.id
+	  	end
 	end
 end
