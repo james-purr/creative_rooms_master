3008e3e510c8c9a8fff2759a4b1ae43ef3ecd143
James Bennett <jamespjbennett@hotmail.com>
2017-03-13 08:00:04 +0000

    add validation

--- creative_rooms_master/app/models/property.rb
+++ creative_rooms_master/app/models/property.rb
@@ -4,12 +4,21 @@ class Property < ApplicationRecord
 	has_many :property_images
 	validates :postcode, :description, :space_for_artist, :user_id,  presence: true
 	validate :must_be_homeowner
+	validate :max_four_featured
 
 
 	def must_be_homeowner
 		if self.user
 			errors.add(:base, 'Property owner cannot be an artist') if !self.user.has_role? "Homeowner"
 		end
+	end
+	
+	def max_four_featured
+		if self.featured
+			if Property.where(featured:true).count >= 4
+				errors.add(:base, 'Maximum number of featured Properties is 4')
+			end
+		end		
 	end
 
 
