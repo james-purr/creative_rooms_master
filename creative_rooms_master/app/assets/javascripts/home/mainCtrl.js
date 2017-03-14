cee7a625dcffccdd1ae3dc45083f2b17e9b828a9
James Bennett <jamespjbennett@hotmail.com>
2017-03-14 13:16:22 +0000

    fixing up and getting property postcode to show up

--- creative_rooms_master/app/assets/javascripts/home/mainCtrl.js
+++ creative_rooms_master/app/assets/javascripts/home/mainCtrl.js
@@ -4,38 +4,8 @@ angular.module('creativeRooms')
 '$timeout', 
 '$mdSidenav', 
 '$log',
-function($scope, $timeout, $mdSidenav, $log){
-    $scope.toggleLeft = buildDelayedToggler('left');
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }
-    }
+'properties',
+function($scope, $timeout, $mdSidenav, $log, properties){
+    $scope.featuredProperties = properties.featuredProperties
+    debugger
 }])
