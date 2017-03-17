e579764aa7ececbfbc6cb1bea3bc8b186df9c33b
James Bennett <jamespjbennett@hotmail.com>
2017-03-17 07:24:42 +0000

    adjustments to maps controller

--- creative_rooms_master/app/assets/javascripts/home/mainCtrl.js
+++ creative_rooms_master/app/assets/javascripts/home/mainCtrl.js
@@ -8,6 +8,4 @@ angular.module('creativeRooms')
 'NgMap',
 function($scope, $timeout, $mdSidenav, $log, properties, NgMap){
     $scope.featuredProperties = properties.featuredProperties
-    $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzyjerX1bvtZ6Hgjn9fLOm0Le_eR4a888"
-    debugger
 }])
--- creative_rooms_master/app/assets/javascripts/maps/mapCtrl.js
+++ creative_rooms_master/app/assets/javascripts/maps/mapCtrl.js
@@ -4,6 +4,9 @@ angular.module('creativeRooms')
 'properties',
 'NgMap',
 function($scope, properties, NgMap){
-    $scope.featuredProperties = properties.featuredProperties
+	// function to get properties(just postcodes) within bounds of map
+	// need to get bounds, and pass that to the factory function so it knows the coordinates to look within
+	// either convert postcodes to lat/lng objects in ruby, or do it in the factory. We basically need a list of lat/lng coordinates to return here
+	// limit the query to 20 properties at a time maybe? Think of ways to limit the load time to get the properties
 
 }])
