11d5569d3d3c248878f60fdfeeb95d8634593582
James Bennett <jamespjbennett@hotmail.com>
2017-05-13 10:34:13 +0100

    a few front end changes to the search results page

--- creative_rooms_master/app/assets/javascripts/search/searchCtrl.js
+++ creative_rooms_master/app/assets/javascripts/search/searchCtrl.js
@@ -10,8 +10,6 @@ angular.module('creativeRooms')
 function($scope, $timeout, $log, $stateParams, $state, properties,$http){
 	$scope.searchString = $stateParams.searchString;
 	$http.get('/search_results/' + $scope.searchString + '.json').then(function(data){
-		debugger
         $scope.properties =  data.data;
     });	
-    debugger
 }]);
