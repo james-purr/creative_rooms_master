9ec8980f49b5e54fa049b6ad37091c5c1c2502e2
James Bennett <jamespjbennett@hotmail.com>
2017-05-14 09:33:41 +0100

    condition for when there is no search results

--- creative_rooms_master/app/assets/javascripts/search/searchCtrl.js
+++ creative_rooms_master/app/assets/javascripts/search/searchCtrl.js
@@ -11,6 +11,10 @@ angular.module('creativeRooms')
 function($scope, $timeout, $log, $stateParams, $state, properties,$http,NgMap){
 	$scope.searchString = $stateParams.searchString;
 	$http.get('/search_results/' + $scope.searchString + '.json').then(function(data){
-        $scope.properties =  data.data;
+        $scope.properties =  data.data[0];
+        $scope.properties = Object.keys($scope.properties).length ? $scope.properties : null
+
+        $scope.noProperties = $scope.properties ? null : 'No rooms found for ' + $scope.searchString + '. Try searching again or get notified when rooms in ' + $scope.searchString + ' become available.'
+        $scope.mapCenter = data.data[1];
     });	
 }]);
--- creative_rooms_master/app/controllers/properties_controller.rb
+++ creative_rooms_master/app/controllers/properties_controller.rb
@@ -12,7 +12,8 @@ class PropertiesController < ApplicationController
 
 	def search_results
 		search_results = Property.near(params["search_term"]).select(:id).map(&:rooms)
+		@search_coordinates = Geocoder.coordinates(params["search_term"])
 		@search_return = Room.room_with_image(search_results)
-		render json: @search_return
+		render json: [@search_return, @search_coordinates]
 	end
 end
