a92c3d4fef9b3e62764cbe742ecab3d40564ca5a
James Bennett <jamespjbennett@hotmail.com>
2017-05-16 20:43:54 +0100

    fixes to get the image slider working

--- creative_rooms_master/app/assets/javascripts/room/roomCtrl.js
+++ creative_rooms_master/app/assets/javascripts/room/roomCtrl.js
@@ -5,23 +5,42 @@ angular.module('creativeRooms')
 '$state',
 '$http',
 'NgMap',
-function($scope, $stateParams, $state, $http, NgMap){
+'$rootScope',
+function($scope, $stateParams, $state, $http, NgMap, $rootScope){
 	var roomId = $stateParams.roomId;
 	$http.get('/rooms/' + roomId + '.json').then(function(data){
 		$scope.room = data.data;
 		$scope.images = data.data.images;
-	});
-	$scope.slickConfig = {
-	    enabled: true,
-	    autoplay: true,
-	    draggable: false,
-	    autoplaySpeed: 3000,
-	    method: {},
-	    event: {
-	        beforeChange: function (event, slick, currentSlide, nextSlide) {
+		$rootScope.viewData = $scope.images;
+
+	    $scope.slickPanels = {
+	      method: {},
+	      dots:true,
+	      infinite: false,
+	      speed: 300,
+	      slidesToShow: 1,
+	      lazyLoad: 'ondemand',
+	      slidesToScroll: 1,
+	      autoPlay: false,
+	      adaptiveHeight: true,
+	      event: {
+	        beforeChange: function() {
+	          console.log("before change called");
 	        },
-	        afterChange: function (event, slick, currentSlide, nextSlide) {
+	        afterChange: function() {
+	          console.log("after called");
 	        }
-	    }
-	};
+	      }
+	    };
+	    $rootScope.updateView = function() {
+	      $rootScope.viewLoaded = false;
+	      $rootScope.viewData.push({
+	        text: "My View" + $rootScope.viewData.length
+	      });
+	      $timeout(function() {
+	        $rootScope.viewLoaded = true;
+	      });
+	    };
+	    $rootScope.viewLoaded = true;
+	});
 }]);
