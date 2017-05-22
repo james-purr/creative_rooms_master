48e45110d96f35efd7d3b8948a5f85110161fd7f
James Bennett <jamespjbennett@hotmail.com>
2017-05-22 20:50:13 +0100

    rails controller logic

--- creative_rooms_master/app/controllers/bookings_controller.rb
+++ creative_rooms_master/app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
 
 	def booking_request
-		binding.pry
+		startDate = Date.parse(params["startDate"])
+		endDate = Date.parse(params["endDate"])
+		room = Room.find(params["room"])
+		Booking.create(start_date: startDate, end_date: endDate, artist_id: current_user.id, homeowner_id: room.property.user.id, room_id: room.id)
 	end
 end
