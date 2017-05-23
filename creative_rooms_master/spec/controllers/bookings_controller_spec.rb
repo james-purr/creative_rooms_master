e8e5a4d9f3246ea3983ce6e8e94ab47e98cbb8ad
James Bennett <jamespjbennett@hotmail.com>
2017-05-23 21:01:33 +0100

    add dashbaord icon

---
+++ creative_rooms_master/spec/controllers/bookings_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe BookingsController, type: :controller do
+
+	let(:parsed_response) { JSON.parse(response.body) }
+
+	describe "GET #booking_request" do
+		
+	end
+end
