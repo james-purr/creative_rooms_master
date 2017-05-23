8d149be3e1c5cbe3d6f91c94e838527eeabfbf59
James Bennett <jamespjbennett@hotmail.com>
2017-05-23 22:18:38 +0100

    comment code

--- creative_rooms_master/spec/requests/bookings_spec.rb
+++ creative_rooms_master/spec/requests/bookings_spec.rb
@@ -2,28 +2,22 @@ require "rails_helper"
 
 RSpec.describe "Bookings creation", :type => :request do
 
+	# put the returned JSON response into a variable
 	let(:parsed_response) { JSON.parse(response.body) }
 	
+	#get artist and homeowner for the booking params
 	artist = (User.with_role :Artist).last
 	homeowner = (User.with_role :Homeowner).last
 	
+	# get current user for the controller
+	user = build(:user)
+    sign_in user	
 
 	it "creates a Booking with the given params" do
-		user = build(:user)
-        sign_in user
         room = create(:room)
         count = Booking.count
 		params = {startDate: Date.today, endDate: Date.today + 7.days, room: room.id, }
 		get "/booking-request", params, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 		expect(Booking.count).to eq(count+1)
-		# expect(response).to render_template(:new)
-
-		# post "/widgets", :widget => {:name => "My Widget"}
-
-		# expect(response).to redirect_to(assigns(:widget))
-		# follow_redirect!
-
-		# expect(response).to render_template(:show)
-		# expect(response.body).to include("Widget was successfully created.")
 	end
 end
